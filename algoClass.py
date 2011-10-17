5d333555fb2b22fea3a0d553b311c0d1417928d4
blacknight0117 <blacKnight0117@gmail.com>
2011-10-17 19:39:46 -0300

    Update algoClass.py

--- algoClass.py
+++ algoClass.py
@@ -4,7 +4,8 @@ class algoClass()
         self.inMemProcs = []
         self.time = 0
         self.toAddProcs = procList
-        
+        self.organizeProcs()
+        self.currentProc = self.inMemProcs[0]
         
     #runs the current process in cpu
     def run(self):
@@ -20,23 +21,37 @@ class algoClass()
     
     #adds a process to memory
     def organizeProcs(self)
-        pass
-        
+        j = len(self.inMemProcs)
+        for i in range(self.toAddProcs)
+            if self.toAddProcs[i].start_time <= self.time
+                if self.time != 0
+                    self.output(self.toaddProcs[i], "Queued")
+                self.inMemProcs[j]= self.toAddProcs[i]
+                #delete self.toAddProcs[i], i--
+        if self.type == "FCFS" || self.type == "RR"
+        elif self.type == "SJF" || self.type == "PSFJ"
+            #organize by inMemProcs.time_req (think about which one is currentProc
+        elif self.type == "Pri"
+            
+    #finds the process given in the self.inMemProcs list
+    #return -1 if not found
+    def findProc(self, givenProc)
     #switches a process into the cpu
+    
+    #check if round robin, else just switch and do normally
     def contextSwitch(self, nextproc):
         pass
         
     #checks if process needs to be switched based on the algorithm
     def checkSwitch(self):
-        if self.type == "FCFS":
-            self.FCFS()
-        elif self.type == "SJF":
-            self.SJF()
+        if self.type == "FCFS" || self.type == "SJF":
+            return 0;
         elif self.type == "PSJF":
-            self.PSJF()
+            if self.findProc(currentProc)!=0
+                self.contextSwitch(self.inMemProcs[0])
         elif self.type == "RR":
-            self.RR()
+            self.contextSwitch(self.inMemProcs[1])
         elif self.type == "PRI":
-            self.PRI()
+            #do whatever
         else:
             print("error, invalid algorithm\n choices are FCFS SJF PSJF RR PRI\n")
